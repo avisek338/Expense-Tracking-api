e11602f838344775fc5267a28a3f2b9a9ccc7263
Avisek Mandal <105858237+avisek338@users.noreply.github.com>
2024-08-04 17:11:15 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,18 @@
 const userModel = require("../models/userModel");
-
+const bcrypt  = require('bcryptjs')
 // login callback
 const loginController = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await userModel.findOne({ email, password });
+    const user = await userModel.findOne({ email});
     if (!user) {
       return res.status(404).send("User Not Found");
     }
+   const isMatch = await bcrypt.compare(password,user.password)
+   if(!isMatch){
+      return res.status(404).send(" incorrect password")
+   }
+
     res.status(200).json({
       success: true,
       user,
@@ -19,6 +24,25 @@ const loginController = async (req, res) => {
     });
   }
 };
+// // login callback
+// const loginController = async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+//     const user = await userModel.findOne({ email, password });
+//     if (!user) {
+//       return res.status(404).send("User Not Found");
+//     }
+//     res.status(200).json({
+//       success: true,
+//       user,
+//     });
+//   } catch (error) {
+//     res.status(400).json({
+//       success: false,
+//       error,
+//     });
+//   }
+// };
 
 //Register Callback
 const registerController = async (req, res) => {
